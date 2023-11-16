a91f12ff0598aa483fbc9f810521224ba1d22e99
Slobodan Komazec <skomazec@xorantech.com>
2023-11-17 00:42:50 +0100

    added profile dropdown

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,25 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@radix-ui/react-dropdown-menu';
+
 const Nav = () => (
   <nav className="h-10 w-full bg-green-500 flex items-center">
     <div className="pl-2">POKEMONS</div>
+    <div className="ml-auto p-10">
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-green-500">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   </nav>
 );
 
