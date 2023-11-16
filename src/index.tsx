e519eaf1fe173fb9f45f3d21634b2b489bbb0ba5
Slobodan Komazec <skomazec@xorantech.com>
2023-11-16 17:28:29 +0100

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './components/theme-provider';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
---
+++ src/pages/Pokemons.tsx
@@ -0,0 +1,60 @@
+import { useTheme } from '@/components/theme-provider';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import React, { useState } from 'react';
+
+const Pokemons: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const { theme } = useTheme();
+  return (
+    <header className="h-screen flex items-center py-8 container">
+      <div className="w-full grid grid-cols-2 gap-4 content-start items-center justify-center">
+        <div className="col-span-2">
+          <div className="text-4xl font-bold">Shadcn CRA Starter</div>
+          <div className="text-lg text-muted-foreground">
+            The advantages of Create-React-App and Shadcn UI, all in one place
+          </div>
+        </div>
+        <Card className="h-72">
+          <CardHeader>
+            <CardTitle>Components</CardTitle>
+            <CardDescription>Adding Components</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="text-muted-foreground text-sm">
+              Step 1: Use the{' '}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://ui.shadcn.com/docs/cli"
+                className="underline"
+              >
+                Shadcn-UI CLI Tool
+              </a>
+            </div>
+            <div className="text-sm p-2 rounded border bg-muted text-muted-foreground">
+              <p className="font-mono">npx shadcn-ui@latest add button</p>
+            </div>
+            <div className="text-muted-foreground text-sm">
+              Step 2: Use it !
+            </div>
+            <Button
+              variant="outline"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              Count is {count}
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </header>
+  );
+};
+
+export default Pokemons;
